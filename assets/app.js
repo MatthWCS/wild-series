26e5acf55181f18a5b4a116fe81a59c78388fcd7
MatthWCS <matthieuhelbert67@gmail.com>
2024-05-21 22:35:53 +0200

    Installation de bootstrap et du sass-bundle

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
+
+import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
