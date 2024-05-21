7a2baffdbad96249304c5d6d65eb8f3fe37b4e23
MatthWCS <matthieuhelbert67@gmail.com>
2024-05-21 21:58:40 +0200

    Ajout du ProgramController et du .idea dans le .gitignore

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProgramController extends AbstractController
+{
+    #[Route('/program/', name: 'program_index')]
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Wild Series Index</body></html>'
+        );
+    }
+}
