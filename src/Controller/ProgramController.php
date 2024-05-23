4cac3690bdd83d99c21bae0a954ecc7503afd309
MatthWCS <matthieuhelbert67@gmail.com>
2024-05-23 11:32:17 +0200

    Création de routes

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,4 +15,10 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+
+    #[Route('/program/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'program_show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id,]);
+    }
 }
