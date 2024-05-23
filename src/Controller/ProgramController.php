dcc48ced16e7ff6b415a83cc8f425b60f385e901
MatthWCS <matthieuhelbert67@gmail.com>
2024-05-23 18:56:23 +0200

    Ajout de la table Program avec les champs title, synopsis et poster

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
@@ -16,7 +17,7 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/program/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'program_show')]
+    #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
     public function show(int $id): Response
     {
         return $this->render('program/show.html.twig', ['id' => $id,]);
