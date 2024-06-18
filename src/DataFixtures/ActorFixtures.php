ce73b3c7b9c76261c9cc68954ae0c1be88d0eedf
Matthieu Helbert <matthieuhelbert67@gmail.com>
2024-06-18 13:09:03 +0200

    Ajout des fixtures , vues et liens

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -2,22 +2,32 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
+use App\Entity\Actor;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class ActorFixtures extends Fixture
+class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
         for ($i = 1; $i <= 10; $i++) {
-            $actor = new Category();
-            $actor->setName($faker->words(2));
+            $actor = new Actor();
+            $actor->setName($faker->name());
+            $actor->addProgram($this->getReference('program_' . $faker->numberBetween(1, 10)));
+            $actor->addProgram($this->getReference('program_' . $faker->numberBetween(1, 10)));
+            $actor->addProgram($this->getReference('program_' . $faker->numberBetween(1, 10)));
+
             $manager->persist($actor);
             $this->addReference('actor_' . $i, $actor);
         }
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [ProgramFixtures::class];
     }
 }
